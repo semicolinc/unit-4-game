32fd0e3e49f411c85185e9d5e62cb3bf5f45223a
colchang <colchang@iu.edu>
2020-02-13 02:05:39 -0500

    Added some jquery elements.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -4,21 +4,57 @@ $(document).ready(function(){
         var initial_char = $(this).attr("id");
 
         if (initial_char === "bulbasaur"){
-            alert("bulbasaur");
+            //make other elements disappear
+            $("#squirtle").css("visibility", "hidden");
+            $("#charmander").css("visibility", "hidden");
+            $("#pikachu").css("visibility", "hidden");
+
+            $("bulbasaur").css("order:", "1");
+            $(this).removeClass("green"); //remove this class to prevent hover
+            // var nick = prompt("Input the nickname for Bulbasaur:")
+
         }
         else if (initial_char === "squirtle"){
-            alert("squirtle");
+            //make other elements disappear
+            $("#charmander").css("visibility", "hidden");
+            $("#bulbasaur").css("visibility", "hidden");
+            $("#pikachu").css("visibility", "hidden");
             
+            $("#squirtle").css("order", "1");
+            $("#bulbasaur").css("order", "2");
+            $("#charmander").css("order", "3");
+            $("#pikachu").css("order", "4");
+            $(this).removeClass("blue");
+            // var nick = prompt("Input the nickname for Bulbasaur:")
+
         }
         else if (initial_char === "charmander"){
-            alert("charmander");
-            
+            //make other elements disappear
+            $("#squirtle").css("visibility", "hidden");
+            $("#bulbasaur").css("visibility", "hidden");
+            $("#pikachu").css("visibility", "hidden");
+
+            $("#squirtle").css("order", "2");
+            $("#bulbasaur").css("order", "3");
+            $("#charmander").css("order", "1");
+            $("#pikachu").css("order", "4");
+            $(this).removeClass("red");
+            // var nick = prompt("Input the nickname for Bulbasaur:")
         }
         else {
-            alert("pikachu");
-            
+            //make other elements disappear
+            $("#squirtle").css("visibility", "hidden");
+            $("#charmander").css("visibility", "hidden");
+            $("#pikachu").css("visibility", "hidden");
+
+            $("#squirtle").css("order", "2");
+            $("#bulbasaur").css("order", "3");
+            $("#charmander").css("order", "4");
+            $("#pikachu").css("order", "1");
+            $(this).removeClass("yellow");
+            // var nick = prompt("Input the nickname for Bulbasaur:")
         }
     })
 
-    
+
 })
